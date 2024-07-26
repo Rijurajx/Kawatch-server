86449e69ab1530d98256a5c81894b89e4ff1b636
Riju Raj Datta <91245104+Rijurajx@users.noreply.github.com>
2024-07-26 18:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": ["https://kawatch-wkqh.vercel.app"]}}, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "https://kawatch-1453r.vercel.app"}})
 
 # Load the model, scaler, and encoders
 model = joblib.load('models/lightgbm_aml_model.pkl')
